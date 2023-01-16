d5ae5028acf64dc8a69eb7d63f29990d612013df
danthecm <etchiedaniel1@gmail.com>
2023-01-16 23:14:37 +0100

    feat: register APIKEY model in admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User, BotModel, OrderModel, TradeProfile
+from .models import User, BotModel, OrderModel, TradeProfile, APIKEYModel
 
 # Register your models here.
-admin.site.register((User, TradeProfile, BotModel, OrderModel))
+admin.site.register((User, TradeProfile, BotModel, OrderModel, APIKEYModel))
