913d75296f97467cd957f97f78c9f5709df24f3a
danthecm <etchiedaniel1@gmail.com>
2023-01-16 23:33:56 +0100

    feat: fix error if api_key does not exist in header

--- api/auth.py
+++ api/auth.py
@@ -3,9 +3,12 @@ from rest_framework.authentication import BaseAuthentication
 from rest_framework.exceptions import AuthenticationFailed
 from .models import APIKEYModel, User
 
+
 class APIKEYAuthentication(BaseAuthentication):
     def authenticate(self, request):
         api_key = request.META.get('HTTP_API_KEY')
+        if not api_key:
+            return None
         my_key = APIKEYModel.verify(api_key)
         if not my_key:
             raise AuthenticationFailed('Invalid API Key for the specified app')
