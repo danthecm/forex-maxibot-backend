6f2f5e2072634406f0ab612244e80154ba963f1d
danthecm <etchiedaniel1@gmail.com>
2023-02-20 13:46:08 +0100

    feat: added profit column for bot model

--- api/models.py
+++ api/models.py
@@ -127,6 +127,7 @@ class BotModel(DateAbtract):
     side = models.CharField(max_length=5, choices=(
         ("buy", "buy"), ("sell", "sell"), ("all", "all")), default="all")
     skip = models.DecimalField(decimal_places=5, max_digits=6, default=0)
+    profit = models.DecimalField(decimal_places=4, max_digits=10, default=0)
 
     def __str__(self):
         return f"{self.owner.username} {self.symbol}"
