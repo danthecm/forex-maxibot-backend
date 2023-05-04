4c2d82e027409f4590a2e67397cab8035e01a4af
danthecm <etchiedaniel1@gmail.com>
2023-05-04 15:23:37 +0100

    feat: create api_key serializer

--- api/serializers.py
+++ api/serializers.py
@@ -1,48 +1,66 @@
 from rest_framework import serializers
-from .models import User, BotModel, OrderModel, TradeProfile
+from .models import User, BotModel, OrderModel, TradeProfile, ApiKeyModel
 
-
-class TradeProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = TradeProfile
-        fields = "__all__"
 
 class SendVerificationSerializer(serializers.Serializer):
     email = serializers.EmailField()
 
-class UserSerializer(serializers.ModelSerializer):
-    trade_profile = TradeProfileSerializer(read_only=True)
-    class Meta:
-        model = User
-        exclude = ("password", "is_staff", "is_superuser", "verification_code")
 
 class RegisteriatonSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ("username", "email", "password", "first_name", "last_name")
 
+
 class LoginSerializer(serializers.Serializer):
-  username = serializers.CharField()
-  password = serializers.CharField()
-        
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+
 class OrderSerializer(serializers.ModelSerializer):
     class Meta:
         model = OrderModel
         fields = "__all__"
 
+
 class BotSerializer(serializers.ModelSerializer):
     orders = OrderSerializer(many=True, read_only=True)
+
     class Meta:
         model = BotModel
         fields = "__all__"
 
-class ActiveUsersSerializer(serializers.ModelSerializer):
-    trade_profile = TradeProfileSerializer(read_only=True)
+
+class TradeProfileSerializer(serializers.ModelSerializer):
     bots = BotSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = TradeProfile
+        fields = "__all__"
+
+
+class UserSerializer(serializers.ModelSerializer):
+    trade_profile = TradeProfileSerializer(read_only=True, many=True)
+
+    class Meta:
+        model = User
+        exclude = ("password", "is_staff", "is_superuser", "verification_code")
+
+
+class ActiveUsersSerializer(serializers.ModelSerializer):
+    trade_profile = TradeProfileSerializer(read_only=True, many=True)
+
     class Meta:
         model = User
         exclude = ("password",)
-        
+
 
 class VerifySerializer(serializers.Serializer):
     code = serializers.IntegerField()
+
+
+class ApiKeySerializer(serializers.ModelSerializer):
+    user_id = serializers.CharField()
+    class Meta:
+        model = ApiKeyModel
+        fields = ("user_id", "name")
