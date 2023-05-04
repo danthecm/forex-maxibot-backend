42f5194142fa337754146c09d7abf8401487e547
danthecm <etchiedaniel1@gmail.com>
2023-05-04 16:19:13 +0100

    feat: all api-key viewset to url

--- api/urls.py
+++ api/urls.py
@@ -1,22 +1,23 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import (UserViewSet, BotViewSet, OrderViewSet, ActiveUsersViewSet, VerifyViewSet,
-                    RegisterationViewSet, LoginViewSet, TradeProfileViewSet, SendVerificationViewSet, LogoutViewSet, TokenRefreshViewSet)
+                    RegisterationViewSet, LoginViewSet, TradeProfileViewSet, SendVerificationViewSet, LogoutViewSet, TokenRefreshViewSet, ApiKeyViewSet)
 
 my_router = DefaultRouter()
 my_router.register("user", UserViewSet, "user")
 my_router.register(r"verify", VerifyViewSet, basename="verify")
-my_router.register("send_verification", SendVerificationViewSet,
-                   basename="send_verification")
+my_router.register("send-verification", SendVerificationViewSet,
+                   basename="send verification")
 my_router.register("register", RegisterationViewSet, basename="register")
 my_router.register('login', LoginViewSet, basename='login')
 my_router.register("logout", LogoutViewSet, basename='logout')
 my_router.register("refresh", TokenRefreshViewSet, basename="refresh")
-my_router.register("trade_profile", TradeProfileViewSet,
-                   basename='trade_profile')
+my_router.register("trade-profile", TradeProfileViewSet,
+                   basename='trade profile')
 my_router.register("bot", BotViewSet, "bot")
 my_router.register("order", OrderViewSet, "order")
-my_router.register("active_users", ActiveUsersViewSet, "active_users")
+my_router.register("active-users", ActiveUsersViewSet, "active users")
+my_router.register("api-key", ApiKeyViewSet, "api key")
 
 
 urlpatterns = [
