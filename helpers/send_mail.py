fd258f5acc3887f2608e6a29d9138a75610527f8
danthecm <etchiedaniel1@gmail.com>
2023-05-08 20:12:54 +0100

    fix: fix styles issues in send_mail html_message

--- helpers/send_mail.py
+++ helpers/send_mail.py
@@ -24,4 +24,4 @@ def send_verification(name, email, url, code):
         <h3>Thanks for registering Please verify your email </h3> \
         <p>Copy the verification code or click the verify button to verify</p> \
         <p> Code: {code}</p> \
-        <a style='padding: 1rem; color: #fff; background-color: #27ae60; border: none; border-radius: 7px; text-decoration: none;' href='{url}'>Verify</a>")
+        <a style='margin: 1rem 0; padding: 1rem; color: #fff; background-color: #27ae60; border: none; border-radius: 7px; text-decoration: none;' href='{url}'>Verify</a>")
